9a5216be8edc4c2aba36c920af6dcbbddebffd4f
Marc000.99 <53879176+MarcoNaik@users.noreply.github.com>
2020-10-07 16:19:08 -0300

    movement

--- Scritps/KinematicBody2D.gd
+++ Scritps/KinematicBody2D.gd
@@ -3,36 +3,31 @@ extends KinematicBody2D
 var target = Vector2()
 var in_target = true
 var linear_vel = Vector2()
-var SPEED = 200
+var SPEED = 1000
 
-var vel : Vector2
-var dashSpeed : float = 1000
+
 var dashRange : float = 100
-var currentDashRange : float = dashRange
-var isDashing : bool = false
-var dashMousePos : Vector2
 
 
 
-func _process(delta):
+func _physics_process(delta):
+	
 	if Input.is_action_just_pressed("dash"):
 		target = get_global_mouse_position()
 		in_target = false
-
-func _physics_process(delta):
+	
 	if not in_target:
 		var diff = target - position
 		var dist = diff.length()
-		
+	
 		if dist > dashRange:
-			target = target.normalized() * dashRange
-			diff = target - position
-			dist = diff.length()
-		
+			target = position + diff.normalized() * dashRange
+			dist = dashRange
+	
 		var vec = diff/dist
 		var target_vel = vec * SPEED
 		var arrived = dist < SPEED * delta
-		
+			
 		if arrived:
 			target_vel = diff
 		else:
@@ -43,7 +38,7 @@ func _physics_process(delta):
 		if get_slide_count() > 0:
 			in_target
 			target = position
-			
+		
 		for i in get_slide_count():
 			var collision = get_slide_collision(i)
 			if collision.collider.is_in_group("player"):
