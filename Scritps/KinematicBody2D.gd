3d2120b108ff470f6692b8a3877583a2fa0ee616
Marco <marcogomezf@gmail.com>
2020-10-28 19:18:53 -0300

    new movement

--- Scritps/KinematicBody2D.gd
+++ Scritps/KinematicBody2D.gd
@@ -40,12 +40,33 @@ func set_hp(value):
 
 func _physics_process(delta):
 	if Input.is_action_just_released("bigger") or Input.is_action_pressed("bigger") :
-		scale = min(scale.x+0.1,SCALE_BIG) * Vector2.ONE
-		linear_vel = move_and_slide(linear_vel)
+		
+		var new_scale = min(scale.x+0.1,SCALE_BIG) * Vector2.ONE
+		
+		var space_state = get_world_2d().direct_space_state
+		var shape: RectangleShape2D = $CollisionShape2D.shape.duplicate()
+		shape.extents *= new_scale
+		var query = Physics2DShapeQueryParameters.new()
+		query.set_shape(shape)
+		query.exclude = [self]
+		query.transform.origin = global_position
+		var result = space_state.intersect_shape(query)
+		var normal = Vector2()
+		for r in result:
+			if r.collider is TileMap:
+				var tilemap: TileMap = r.collider
+				# TODO make an inverse relationship
+				normal += (global_position - tilemap.map_to_world(r.metadata)).normalized()
+		if result.size() > 0:
+			target = global_position + normal.normalized() * 10
+			in_target = false
+	#		position += normal.normalized() * 4
+		scale = new_scale
+	#		linear_vel = move_and_slide(linear_vel)
 		
 	if Input.is_action_just_released("smaller") or Input.is_action_pressed("smaller"):
 		scale = max(scale.x-0.1,SCALE_SMALL) * Vector2.ONE
-		linear_vel = move_and_slide(linear_vel)
+#		linear_vel = move_and_slide(linear_vel)
 
 	if Input.is_action_just_pressed("dash"):
 		start_dash()
@@ -71,7 +92,6 @@ func _physics_process(delta):
 		linear_vel = move_and_slide(linear_vel)
 		
 		if get_slide_count() > 0:
-			in_target
 			target = position
 		
 		for i in get_slide_count():
