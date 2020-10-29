93f79c9ef7377feb2cfe898be847cfa96c5af8c4
pedropopelka <71889009+pedropopelka@users.noreply.github.com>
2020-10-29 19:53:55 -0300

    Arreglos animaciones

--- Scritps/Enemy.gd
+++ Scritps/Enemy.gd
@@ -4,10 +4,11 @@ onready var player = get_node("/root/Main/Character")
 var SPEED = 100
 var linear_vel = Vector2()
 var hp = 100 setget set_hp
-var damage = 1
+var damage = 0.2
 
 func receive_damage(amount):
-	set_hp(hp-amount)
+	if hp>0:
+		set_hp(hp-amount)
 
 func set_hp(value):
 	hp = clamp(value, 0, 100)
@@ -18,21 +19,25 @@ func set_hp(value):
 		$AnimationPlayer.current_animation = "dies"
 
 func _physics_process(delta):
+	
 	var target = player.position
 	var diff = target - position
 	var target_vel = diff.normalized() * SPEED
-	
-	linear_vel = lerp(linear_vel, target_vel, 0.5)
-	linear_vel = move_and_slide(linear_vel)
+	if hp>0:
+		linear_vel = lerp(linear_vel, target_vel, 0.5)
+		linear_vel = move_and_slide(linear_vel)
+	else:
+		SPEED=0
+
 
 func _process(delta):
-	for i in get_slide_count():
-		var collision = get_slide_collision(i)
-		if hp>0:
+	if hp>0:
+		for i in get_slide_count():
+			var collision = get_slide_collision(i)
+			
 			if collision.collider.is_in_group("player"):
 				collision.collider.receive_damag(damage)
 				$AnimationPlayer.current_animation = "attack"
-		else:
-			$AnimationPlayer.current_animation = "dies"
+	
 
 
