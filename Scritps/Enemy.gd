dbce7e0946625bf3ec32500aa7b4b43b94159cb8
Marco <marcogomezf@gmail.com>
2020-10-29 23:24:18 -0300

    prototype enemy 2"

--- Scritps/Enemy.gd
+++ Scritps/Enemy.gd
@@ -1,11 +1,10 @@
 extends KinematicBody2D
 
-onready var player = get_node("/root/Main/Character")
+#onready var player = get_node("/root/Main/Character")
 var SPEED = 100
 var linear_vel = Vector2()
 var hp = 100 setget set_hp
 var damage = 0.2
-var dead = false
 
 func receive_damage(amount):
 	if hp>0:
@@ -20,7 +19,7 @@ func set_hp(value):
 		$AnimationPlayer.current_animation = "dies"
 
 func _physics_process(delta):
-	
+
 	var target = player.position
 	var diff = target - position
 	var target_vel = diff.normalized() * SPEED
@@ -33,21 +32,14 @@ func _physics_process(delta):
 
 func _process(delta):
 	if hp>0:
-		var near_player = false
 		for i in get_slide_count():
 			var collision = get_slide_collision(i)
 			
 			if collision.collider.is_in_group("player"):
 				collision.collider.receive_damag(damage)
-				near_player = true
-		if near_player:
-			$AnimationPlayer.current_animation = "attack"
-		else:
-			$AnimationPlayer.current_animation = "idle"
-	elif not dead:
-		$AnimationPlayer.play("dies")
-		dead = true
-		
+				$AnimationPlayer.current_animation = "attack"
+			else:
+				$AnimationPlayer.current_animation = "dies"
 			
 	if linear_vel.x < 0: 
 		$Sprite.flip_h = true
--- Scritps/Enemy2.gd
+++ Scritps/Enemy2.gd
@@ -1,12 +1,24 @@
 extends KinematicBody2D
 
+
 onready var player = get_node("/root/Main/Character")
-var SPEED = 100
+
+var SPEED = 50
 var linear_vel = Vector2()
 var hp = 100 setget set_hp
 var damage = 0.2
 
-var coldown
+var coldown = 2.5
+
+var timer =0
+
+func shoot():
+	var proyectile = load("res://Scene/Bullet_1.tscn")
+	for i in range(12):
+		var bullet = proyectile.instance()
+		bullet.set_rotation_degrees(i*360/12)
+		add_child_below_node(get_tree().get_root().get_node("Main"),bullet)
+
 
 func receive_damage(amount):
 	if hp>0:
@@ -21,7 +33,6 @@ func set_hp(value):
 		$AnimationPlayer.current_animation = "dies"
 
 func _physics_process(delta):
-	
 	var target = player.position
 	var diff = target - position
 	var target_vel = diff.normalized() * SPEED
@@ -33,6 +44,11 @@ func _physics_process(delta):
 
 
 func _process(delta):
+	
+	timer +=delta
+	if timer>coldown:
+		shoot()
+		timer=0
 	
 	if linear_vel.x < 0: 
 		$Sprite.flip_h = true
